cf4088dfd9967affacaaa49592a1eef4b6c2ef5b
David Islam <davidanatolie@Davids-MacBook-Pro.local>
2017-08-29 06:58:23 -0700

    Created Pokemon class

---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,33 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by David Islam on 2017-08-29.
+//  Copyright © 2017 David Islam. All rights reserved.
+//
+
+import Foundation
+
+
+class Pokemon {
+    
+    private var _name: String
+    private var _pokedexId: Int
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+    var name: String {
+        get {
+            return _name
+        }
+    }
+    
+    var pokedexId: Int {
+        get {
+            return _pokedexId
+        }
+    }
+}
